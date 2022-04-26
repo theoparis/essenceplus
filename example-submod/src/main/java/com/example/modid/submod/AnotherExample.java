c884139c46c91940502f1bd949cc73124fdfb3d4
Theo Paris <theoplaysmc@gmail.com>
2022-04-26 15:12:37 -0700

    Initial commit

---
+++ example-submod/src/main/java/com/example/modid/submod/AnotherExample.java
@@ -0,0 +1,20 @@
+package com.example.modid.submod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * Another Example Minecraft mod.
+ */
+public class AnotherExample implements ModInitializer
+{
+    public static final Logger LOGGER = LogManager.getLogger("modid-sub");
+
+    @Override
+    public void onInitialize()
+    {
+        LOGGER.info("Another Example is getting ready...");
+    }
+}
